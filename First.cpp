4aa0e36bcdbb28a382252683c15112655ad942bc
mkcodes21 <mohitkumar2214030@gmail.com>
2026-08-21 13:56:33 +0530

    first comit

---
+++ First.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello Coder Army"<<endl;
+    // Variable declaration
+    int a;
+    a = 12; // Variable initialization
+    int b = 22;
+    cout <<b<<endl;
+
+    char c = 'b';
+    cout << c << endl;
+
+    float f = 10.34;
+    cout << f << endl;
+
+    double d = 10.34837744;
+    cout << d << endl;
+
+    bool isTrue = true;
+    cout << isTrue << endl;
+
+    string str = "Mohit Kumar";
+    cout << str << endl;
+}
---
+++ if_else.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int package = 15;
+    if (package >= 10) {
+        cout << "Offer Accepted" << endl;
+    }
+    else {
+        cout << "Offer Rejected" << endl;
+    }
+}
